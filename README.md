6054c58f4ca20a8b0075191d5f91308dd86c40d8
Slava <>
2021-09-17 23:42:55 +0300

    Upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# magento_custom_csp
+Simple Magento 2 module for CSP with sample csp_whitelist.xml file
---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Copyright © Experius B.V. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Custom_Csp', __DIR__);
